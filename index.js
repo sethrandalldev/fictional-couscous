abaadd7259e363345a614f1698bee39d3634564e
Seth Randall <sethjrandall@gmail.com>
2022-01-09 09:29:51 -0500

    [ADDED] custom auth with argon2

--- index.js
+++ index.js
@@ -1,125 +1,104 @@
 const express = require("express");
-const app = express();
-const PORT = 8000;
-const { Client } = require("pg");
+const bodyParser = require("body-parser");
+const argon2 = require("argon2");
 const cors = require("cors");
-const admin = require("firebase-admin");
-const serviceAccount = require("./project-tracker-9ec23-firebase-adminsdk-16f4u-9a747d97af.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-console.log("before new client");
-const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "",
-  port: 5555,
-});
+const app = express();
 
-client.connect();
-
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/:userId/workspaces", (req, res) => {
-  const query = "SELECT * FROM workspaces WHERE created_by = $1";
-  const values = [req.params.userId];
-  client.query(query, values, (err, data) => {
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(400);
-    } else {
-      const workspaces = data.rows;
-      return res.status(200).send(workspaces);
-    }
-  });
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "Seth",
+      email: "seth@gmail.com",
+      password: "cookies",
+      created: new Date(),
+    },
+    {
+      id: "124",
+      name: "John",
+      email: "john@gmail.com",
+      password: "bananas",
+      created: new Date(),
+    },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.json(database.users);
 });
 
-app.post("/workspaces", (req, res) => {
-  const query =
-    "INSERT INTO workspaces(title, description, created_by) VALUES($1, $2, $3) RETURNING *";
-  const values = [req.body.title, req.body.description, req.body.userId];
-
-  client.query(query, values, (err, data) => {
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(400);
-    } else {
-      const workspace = data.rows[0];
-      return res.status(200).send(workspace);
+app.post("/login", async (req, res) => {
+  if (req.body.email === database.users[2].email) {
+    try {
+      if (await argon2.verify(database.users[2].password, req.body.password)) {
+        res.json("success");
+      } else {
+        res.status(404).json("Login failed; Invalid user ID or password.");
+      }
+    } catch (err) {
+      res.status(400).json("Login failed; server error.");
     }
-  });
+  } else {
+    res.status(400).json("error logging in");
+  }
 });
 
-app.get("/workspaces/:id", (req, res) => {
-  const query = "SELECT * FROM workspaces WHERE id = $1";
-  const values = [req.params.id];
-
-  client.query(query, values, (err, data) => {
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(400);
-    } else {
-      const workspace = data.rows[0];
-      return res.status(200).send(workspace);
-    }
-  });
+app.post("/register", async (req, res) => {
+  const { email, name, password } = req.body;
+  try {
+    const hash = await argon2.hash(password);
+    database.users.push({
+      id: "125",
+      name,
+      email,
+      password: hash,
+      created: new Date(),
+    });
+    res.json(database.users[database.users.length - 1]);
+  } catch (err) {
+    res.status(400).json("Error registering user.");
+  }
 });
 
-app.patch("/workspaces/:id", (req, res) => {
-  const query =
-    "UPDATE workspaces SET title = $1, description = $2 WHERE id = $3 RETURNING *";
-  const values = [req.body.title, req.body.description, req.params.id];
-
-  client.query(query, values, (err, data) => {
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(400);
-    } else {
-      const workspace = data.rows[0];
-      return res.status(200).send(workspace);
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      res.json(user);
     }
   });
+  if (!found) {
+    res.status(404).json("no such user");
+  }
 });
 
-app.delete("/workspaces/:id", (req, res) => {
-  const query = "DELETE FROM workspaces WHERE id = $1";
-  const values = [req.params.id];
-
-  client.query(query, values, (err, data) => {
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(400);
-    } else {
-      return res.sendStatus(200);
-    }
-  });
+app.listen(4000, () => {
+  console.log("App is running on port 4000");
 });
 
-app.post("/tickets", (req, res) => {
-  const query =
-    "INSERT INTO tickets(title, assigned_to, created_by, status, priority, project_id, workspace_id) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *";
-  const values = [
-    req.body.title,
-    req.body.assignedTo,
-    req.body.createdBy,
-    req.body.status,
-    req.body.priority,
-    req.body.projectId,
-    req.body.workspaceId,
-  ];
-
-  client.query(query, values, (err, data) => {
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(400);
-    } else {
-      const ticket = data.rows[0];
-      return res.status(200).send(ticket);
-    }
-  });
-});
-
-app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
+/*
+/ --> res = this is working
+/login --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/projects/:projectId --> GET = project
+/projects --> POST = project
+/projects --> GET = projects
+/workspaces/:workspaceId --> GET = workspace
+/workspaces --> POST = workspace
+/workspaces --> GET = workspaces
+/tickets/:ticketId --> GET = ticket
+/tickets --> POST = ticket
+/tickets --> GET = tickets
+/tickets/:ticketId/comments --> POST = comment
+/tickets/:ticketId/comments --> GET = comments
+/workspaces/:workspaceId/:userId --> POST = workspace_user
+/workspaces/:workspacedId/:userId --> GET = workspace_user
+/workspaces/:workspacedId/users --> GET = workspace_users
+*/
